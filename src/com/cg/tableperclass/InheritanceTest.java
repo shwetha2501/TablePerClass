e514d5d07598454212c35d91e08853cc8a4e4672
swetha <shwethabs25@gmail.com>
2022-08-09 09:45:44 +0530

    first commit

---
+++ src/com/cg/tableperclass/InheritanceTest.java
@@ -0,0 +1,41 @@
+package com.cg.tableperclass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		//Create One Employee
+		Employee employee = new Employee();
+		employee.setName("Lekha");
+		employee.setSalary(5000);
+		employee.setName("swetha");
+		employee.setSalary(2000);
+		em.persist(employee);
+		
+		//Create One Manager
+		Manager manager= new Manager();
+		manager.setName("Vikas");
+		manager.setSalary(6000);
+		manager.setDepartmentName("C2TC");
+		em.persist(manager);
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added One Employee and Manager");
+		
+		em.close();
+		
+		factory.close();
+
+	}
+
+}
---
+++ src/com/cg/tableperclass/Manager.java
@@ -0,0 +1,25 @@
+package com.cg.tableperclass;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mgr_store")
+public class Manager extends Employee{
+
+	
+	
+	private String DepartmentName;
+
+	public String getDepartmentName() {
+		return DepartmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		DepartmentName = departmentName;
+	}
+
+
+
+
+}
